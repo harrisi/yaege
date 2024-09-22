69a1a89827ec8be25a0022935fb7c316da517d04
Ian Harris <ian@fofgof.xyz>
2024-09-22 10:46:35 -0700

    remove generated hello world

--- lib/yaege.ex
+++ lib/yaege.ex
@@ -1,18 +1,2 @@
 defmodule Yaege do
-  @moduledoc """
-  Documentation for `Yaege`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Yaege.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
